c712eb20dabf1d94cbee1b3df4b8cc175e86ee25
Mehmet Eren Kala <43720526+Mekala02@users.noreply.github.com>
2023-03-02 18:06:10 +0300

    Initial Commit

---
+++ PPM_Reader.ino
@@ -0,0 +1,35 @@
+#define PPM_PIN 3
+#define PPM_CHANNELS 8
+
+// current time, previous time
+unsigned long int t, pt = 0;
+// channel no, time differance
+int c, d = 0;
+int ch[15];
+
+void Read_Channels();
+
+void setup() {
+    pinMode(PPM_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PPM_PIN), Read_Channels, FALLING);
+}
+
+void loop() {
+  for (int i = 0; i < PPM_CHANNELS; i++){
+    Serial.print(channel[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
+  delay(10);
+}
+
+// Reading ppm signal
+void Read_Channels(){
+    t = micros();
+    d = t - pt;
+    if (d > 3000)
+        c = 0;
+    ch[c] = d;
+    c ++;
+    pt = t;
+}
